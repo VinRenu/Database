ec4cc6a70d1591a38cf5a9e1ff6f35333fe9460f
VinRenu <47265472+VinRenu@users.noreply.github.com>
2020-11-27 15:08:29 +0530

    Update Version1.bat

--- Version1.bat
+++ Version1.bat
@@ -3,5 +3,5 @@ set file=version.txt
 for /f "tokens=*" %%A in (%file%) do (echo %%A)
 set /p Build=<version.txt
 echo %Build%
-ren StorageAdvisor.zip StorageAdvisor-%Build%.zip
+ren StorageAdvisor.zip StorageAdvisor%Build%
 pause
